7fefe641111425ee5d1256fd7b367f3ecdb1e194
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2025-08-01 18:16:37 +0900

    ✨ OpenAIを使ってテキスト生成するクライアントを書いた

--- packages/cdk/lambda/utils/liteLlmClient.ts
+++ packages/cdk/lambda/utils/liteLlmClient.ts
@@ -5,6 +5,42 @@ import {
   Model,
   UnrecordedMessage,
 } from 'generative-ai-use-cases';
+import OpenAI from 'openai';
+import { streamingChunk } from './streamingChunk';
+import { StopReason } from '@aws-sdk/client-bedrock-runtime';
+
+const initOpenAIClient = () => {
+  return new OpenAI({
+    baseURL: 'http://localhost:4000', // TODO: あとでどうにかする
+    apiKey: '',
+  });
+};
+
+const createOpenAIChatCompletionMessages = (messages: UnrecordedMessage[]) => {
+  return messages.map((message) => {
+    return {
+      role: message.role,
+      content: message.content,
+    };
+  });
+};
+
+const convertFinishReason = (
+  reason: 'stop' | 'length' | 'tool_calls' | 'content_filter' | 'function_call'
+) => {
+  switch (reason) {
+    case 'stop':
+      return StopReason.STOP_SEQUENCE;
+    case 'length':
+      return StopReason.MAX_TOKENS;
+    case 'tool_calls' || 'function_call':
+      return StopReason.TOOL_USE;
+    case 'content_filter':
+      return StopReason.CONTENT_FILTERED;
+    default:
+      return 'error';
+  }
+};
 
 const liteLlmClient: ApiInterface = {
   invoke: async function (
@@ -12,15 +48,51 @@ const liteLlmClient: ApiInterface = {
     messages: UnrecordedMessage[],
     id: string
   ): Promise<string> {
-    throw new Error('Function not implemented.');
+    const client = initOpenAIClient();
+
+    const openAIMessages = createOpenAIChatCompletionMessages(messages);
+
+    const completion = await client.chat.completions.create({
+      model: model.modelId,
+      messages: openAIMessages,
+    });
+
+    return completion.choices[0].message.content ?? '';
   },
-  invokeStream: function (
+  invokeStream: async function* (
     model: Model,
     messages: UnrecordedMessage[],
     id: string,
     idToken?: string | undefined
   ): AsyncIterable<string> {
-    throw new Error('Function not implemented.');
+    const client = initOpenAIClient();
+
+    const openAIMessages = createOpenAIChatCompletionMessages(messages);
+
+    const completion = await client.chat.completions.create({
+      model: model.modelId,
+      messages: openAIMessages,
+      stream: true,
+    });
+
+    for await (const chunk of completion) {
+      if (!chunk) {
+        break;
+      }
+
+      if (chunk.choices[0].finish_reason) {
+        const stopReason = convertFinishReason(chunk.choices[0].finish_reason);
+
+        yield streamingChunk({
+          text: '',
+          stopReason: stopReason,
+        });
+      }
+
+      yield streamingChunk({
+        text: chunk.choices[0].delta.content ?? '',
+      });
+    }
   },
   generateImage: function (
     model: Model,
