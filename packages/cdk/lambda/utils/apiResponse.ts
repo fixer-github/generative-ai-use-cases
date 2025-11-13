f1378213743627edc0752197e765d068facf94da
Y.Muro <165998973+muro-yuta24-fixer@users.noreply.github.com>
2025-11-13 17:55:18 +0900

    :art: 関数の順番を入れ替え

--- packages/cdk/lambda/utils/apiResponse.ts
+++ packages/cdk/lambda/utils/apiResponse.ts
@@ -6,23 +6,6 @@ import { APIGatewayProxyResult } from 'aws-lambda';
 import { CORS_HEADERS } from '@generative-ai-use-cases/common';
 
 /**
- * Create Lambda error response
- * @param statusCode HTTP status code
- * @param message Error Message
- * @returns Lambda response
- */
-export function createErrorResponse(
-  statusCode: number,
-  message: string
-): APIGatewayProxyResult {
-  return {
-    statusCode,
-    headers: CORS_HEADERS,
-    body: JSON.stringify({ message: message }),
-  };
-}
-
-/**
  * Create Lambda success response
  * @param statusCode HTTP status code
  * @param body Response body
@@ -36,5 +19,22 @@ export function createSuccessResponse<TBody>(
     statusCode,
     headers: CORS_HEADERS,
     body: JSON.stringify(body),
+  };
+}
+
+/**
+ * Create Lambda error response
+ * @param statusCode HTTP status code
+ * @param message Error Message
+ * @returns Lambda response
+ */
+export function createErrorResponse(
+  statusCode: number,
+  message: string
+): APIGatewayProxyResult {
+  return {
+    statusCode,
+    headers: CORS_HEADERS,
+    body: JSON.stringify({ message: message }),
   };
 }
