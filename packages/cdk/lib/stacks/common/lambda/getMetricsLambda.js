c6302ef5dc8d9f4d5284e4a3d88fb9a115d8b4b9
nanjo.yuki <nanjo.yuki@fixer.co.jp>
2025-12-25 15:17:33 +0900

    add: デイリーでデータを集計するロジックを追加

--- packages/cdk/lib/stacks/common/lambda/getMetricsLambda.js
+++ packages/cdk/lib/stacks/common/lambda/getMetricsLambda.js
@@ -13,8 +13,10 @@ const AUTH_USAGE_EVENTS_TABLE_NAME = process.env.AUTH_USAGE_EVENTS_TABLE_NAME;
 const METRIC_NAMESPACE = 'BIMetrics/DynamoDB';
 const USER_COUNT_METRIC_NAME = 'UsersCount';
 const USER_COUNT_MONTHLY_ACTIVE_METRIC_NAME = 'UsersCountMonthlyActive';
+const USER_COUNT_DAILY_ACTIVE_METRIC_NAME = 'UsersCountDailyActive';
 const USER_COUNT_USAGE_LIMIT_REACHED_METRIC_NAME = 'UsersCountUsageLimitReached';
-const LLM_USAGE_COUNT_METRIC_NAME = 'LLMUsageCount';
+const LLM_USAGE_COUNT_MONTHLY_METRIC_NAME = 'LLMUsageMonthlyCount';
+const LLM_USAGE_COUNT_DAILY_METRIC_NAME = 'LLMUsageDailyCount';
 
 // ユーザーテーブルの件数を取得する関数
 async function getUserCount() {
@@ -50,29 +52,39 @@ async function getUserCount() {
 }
 
 // 今月チャット機能を一度でも使用したユーザー、30回利用しきったユーザー数を取得する関数
-// [<月間アクティブユーザー数>, <利用上限に達したユーザー数>]の形でレスポンスする
+// [<月間アクティブユーザー数>, <日次アクティブユーザー数>, <利用上限に達したユーザー数>, <月次のLLM使用回数>, <日次のLLM使用回数>]の形でレスポンスする
 async function getActiveOrLimitReachedUserCount() {
   if (!AUTH_USAGE_EVENTS_TABLE_NAME) {
     throw new Error('AUTH_USAGE_EVENTS_TABLE_NAME environment variable is not set.');
   }
 
   const searchPrefix = getCurrentYearMonthInJST(); // "YYYY-MM" 形式
-  const startTime = getPeriodStartTime("monthly");
+  const startTimeMonthly = getPeriodStartTime("monthly");
+  const startTimeDaily = getPeriodStartTime("daily");
   const endTime = Date.now();
-  let llmUsageCount = 0;
+  let llmUsageCountMonthly = 0;
+  let llmUsageCountDaily = 0;
 
   // QueryCommand のパラメータ設定
   let scanParams = {
     TableName: AUTH_USAGE_EVENTS_TABLE_NAME,
     // 必要な属性のみ取得: featureIdと userId 属性
-    ProjectionExpression: "userId, featureId",
+    ProjectionExpression: "userId, featureId, #ts",
+    ExpressionAttributeNames: {
+      "#ts": "timestamp" // "#ts" という別名で "timestamp" という予約語をマッピング
+    },
     // timestampが今月のものでフィルタリング
-    FilterConditionExpression: `timestamp BETWEEN ${startTime} AND ${endTime}`,
+    FilterConditionExpression: `timestamp BETWEEN ${startTimeMonthly} AND ${endTime}`,
     Limit: 1000 // 1回のScanで取得する最大件数
   };
 
   let lastEvaluatedKey;
-  const usages = {}; // 集計結果を格納
+  const usagesMonthly = {}; // 月次の集計結果を格納
+  const usagesDaily = {}; // 日次の集計結果を格納
+
+  let activeUserCountMonthly = 0;
+  let activeUserCountDaily = 0;
+
   try {
     do {
       if (lastEvaluatedKey) {
@@ -89,12 +101,21 @@ async function getActiveOrLimitReachedUserCount() {
         const userId = item["userId"].S;
         // userId をキーとして、データ個数を保持
         // Todo: featureId ごとに分ける場合はここを修正
-        if (usages[userId]) {
-          usages[userId]++;
+        if (usagesMonthly[userId]) {
+          usagesMonthly[userId]++;
         } else {
-          usages[userId] = 1;
+          usagesMonthly[userId] = 1;
         }
-        llmUsageCount++;
+        llmUsageCountMonthly++;
+        if (item["timestamp"].N >= startTimeDaily) {
+          llmUsageCountDaily++;
+          if (usagesDaily[userId]) {
+            usagesDaily[userId]++;
+          } else {
+            usagesDaily[userId] = 1;
+            activeUserCountDaily++;
+          }
+        }
       }
       lastEvaluatedKey = response.LastEvaluatedKey;
     } while (lastEvaluatedKey);
@@ -103,19 +124,18 @@ async function getActiveOrLimitReachedUserCount() {
     throw err;
   }
 
-  let monthlyActiveUserCount = 0;
   let usageLimitedUserCount = 0;
   // カウント結果をもとに、月間アクティブユーザー数と利用上限に達したユーザー数を計算
-  for (const usage in usages) {
-    console.log(`User ${usage} has count ${usages[usage]} for month ${searchPrefix}`);
-    monthlyActiveUserCount += 1;
+  for (const usage in usagesMonthly) {
+    // console.log(`User ${usage} has count ${usagesMonthly[usage]} for month ${searchPrefix}`);
+    activeUserCountMonthly++;
     // Todo: 利用上限の閾値はテーブルから取ってくるようにする
-    if (usages[usage] >= 30) {
-      usageLimitedUserCount += 1;
+    if (usagesMonthly[usage] >= 30) {
+      usageLimitedUserCount++;
     }
   }
 
-  return [monthlyActiveUserCount, usageLimitedUserCount, llmUsageCount];
+  return [activeUserCountMonthly, activeUserCountDaily, usageLimitedUserCount, llmUsageCountMonthly, llmUsageCountDaily];
 }
 
 // メトリクスをCloudWatchに公開する関数
@@ -177,16 +197,20 @@ export async function handler(event, context) {
     // 9タイプを登録したユーザー数を取得・メトリクスを公開
 
     // 少なくとも一度チャットまたはAIコンテンツ機能を利用したユーザー数、AIコンテンツ機能を30回使い切ったユーザー数を取得・メトリクスを公開
-    const [monthlyActiveUserCount, usageLimitedUserCount, llmUsageCount] = await getActiveOrLimitReachedUserCount();
-    await publishMetric(USER_COUNT_MONTHLY_ACTIVE_METRIC_NAME, monthlyActiveUserCount);
-    console.log(`Published CloudWatch metric ${METRIC_NAMESPACE}/${USER_COUNT_MONTHLY_ACTIVE_METRIC_NAME} = ${monthlyActiveUserCount}`);
+    const [activeUserCountMonthly, activeUserCountDaily, usageLimitedUserCount, llmUsageCountMonthly, llmUsageCountDaily] = await getActiveOrLimitReachedUserCount();
+    await publishMetric(USER_COUNT_MONTHLY_ACTIVE_METRIC_NAME, activeUserCountMonthly);
+    console.log(`Published CloudWatch metric ${METRIC_NAMESPACE}/${USER_COUNT_MONTHLY_ACTIVE_METRIC_NAME} = ${activeUserCountMonthly}`);
+    await publishMetric(USER_COUNT_DAILY_ACTIVE_METRIC_NAME, activeUserCountDaily);
+    console.log(`Published CloudWatch metric ${METRIC_NAMESPACE}/${USER_COUNT_DAILY_ACTIVE_METRIC_NAME} = ${activeUserCountMonthly}`);
     await publishMetric(USER_COUNT_USAGE_LIMIT_REACHED_METRIC_NAME, usageLimitedUserCount);
     console.log(`Published CloudWatch metric ${METRIC_NAMESPACE}/${USER_COUNT_USAGE_LIMIT_REACHED_METRIC_NAME} = ${usageLimitedUserCount}`);
-    await publishMetric(LLM_USAGE_COUNT_METRIC_NAME, llmUsageCount);
-    console.log(`Published CloudWatch metric ${METRIC_NAMESPACE}/${LLM_USAGE_COUNT_METRIC_NAME} = ${llmUsageCount}`);
+    await publishMetric(LLM_USAGE_COUNT_MONTHLY_METRIC_NAME, llmUsageCountMonthly);
+    console.log(`Published CloudWatch metric ${METRIC_NAMESPACE}/${LLM_USAGE_COUNT_MONTHLY_METRIC_NAME} = ${llmUsageCountMonthly}`);
+    await publishMetric(LLM_USAGE_COUNT_DAILY_METRIC_NAME, llmUsageCountDaily);
+    console.log(`Published CloudWatch metric ${METRIC_NAMESPACE}/${LLM_USAGE_COUNT_DAILY_METRIC_NAME} = ${llmUsageCountDaily}`);
 
 
-    return { userCount, monthlyActiveUserCount, usageLimitedUserCount, llmUsageCount };
+    return { userCount, activeUserCountMonthly, activeUserCountDaily, usageLimitedUserCount, llmUsageCountMonthly, llmUsageCountDaily };
   } catch (err) {
     console.error('Error counting items or publishing metric:', err);
     throw err;
